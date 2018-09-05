c4aa9e4da0496df919b465361653c5ff6a7222dc
Jason To <jasto@ucdavis.edu>
2018-09-05 18:47:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 var playist = { Kaskade: "Last Chance",
-              { Illenium: "Beautiful Creatres",
-              { Gryffin: "Tie Me down"
+               Illenium: "Beautiful Creatres",
+               Gryffin: "Tie Me down"
 }
 
-function updatePlaylist(playlist, 'artist', 'song_title'){
-  return
+function updatePlaylist(playlist, artist, song_title){
+  playlist.Flume = 'Fire';
+  return playlist;
 }
