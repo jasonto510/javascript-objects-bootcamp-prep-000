f1a587e68e0962334c261f0175e8bd3a8e95aaad
Jason To <jasto@ucdavis.edu>
2018-09-05 18:49:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,14 @@
 var playist = { Kaskade: "Last Chance",
                Illenium: "Beautiful Creatres",
                Gryffin: "Tie Me down"
-}
+};
 
 function updatePlaylist(playlist, artist, song_title){
   playlist.Flume = 'Fire';
+  return playlist;
+}
+
+function removeFromPlaylist(playst, artist){
+  delete playlist.Gryffin;
   return playlist;
 }
